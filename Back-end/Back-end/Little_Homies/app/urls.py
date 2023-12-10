262ec97aa30f7579cecf5430f4623e79a1b407d9
Julián Dorao <133274347+Juliandavidlp@users.noreply.github.com>
2023-12-09 22:53:54 -0300

    Delete Back-end directory

--- Back-end/Back-end/Little_Homies/app/urls.py
+++
@@ -1,9 +0,0 @@
-#Acá defino las rutas que van a permitir mi aplicación.
-
-
-from django.urls import path, re_path, include
-from . import views
-
-urlpatterns = [
-    path('', views.index,name='inicio_app'),
-]
