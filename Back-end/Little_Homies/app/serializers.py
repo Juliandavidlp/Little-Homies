b1e102f0077adbd74c685f25a01a678077cc5ed5
Julián Dorao <133274347+Juliandavidlp@users.noreply.github.com>
2023-12-10 23:00:38 -0300

    Django

---
+++ Back-end/Little_Homies/app/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from app.models import mascota
+
+
+class MascotaSerializer (serializers.ModelSerializer):
+
+    class Meta:
+        model=mascota #El modelo con el que se va a corresponder el serializador...
+
+        #Entonces defino los campos de la clase mascota que quiero serializar:
+        fields=['id','title','raza','año_de_nacimiento','banner']
---
+++ Back-end/Little_Homies/app/urls.py
@@ -0,0 +1,13 @@
+#Acá defino las rutas que van a permitir mi aplicación.
+
+from django.urls import path, re_path, include
+from . import views
+
+
+urlpatterns = [
+    path('', views.index,name='inicio_app'),
+    path('mascotas/', views.get_mascotas,name='getmascotas_app'),
+    path('movies/<int:id>/', views.detail_mascota),   
+]
+
+
